5883a62aed91fa91554b1199a8f9eb7c4551b1ba
Connacher Murphy <connacher.murphy@gmail.com>
2025-12-27 11:58:24 -0700

    Initialize unwrapped Google call and add example code from cookbook

---
+++ examples/unwrapped_google.py
@@ -0,0 +1,126 @@
+import logging
+
+from dotenv import load_dotenv
+from google import genai
+
+logging.basicConfig(level=logging.INFO)
+
+if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
+
+    load_dotenv()
+
+    client = genai.Client()
+
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents="Can you introduce yourself?",
+    )
+
+    print(response.text)
+
+# api_key = os.environ.get("GEMINI_API_KEY")
+
+
+# gemini-3-flash-preview
+
+# from google import genai
+# from google.genai import types
+
+# client = genai.Client()
+# prompt = "What is the sum of the first 50 prime numbers?"
+# response = client.models.generate_content(
+#     model="gemini-2.5-pro",
+#     contents=prompt,
+#     config=types.GenerateContentConfig(
+#         thinking_config=types.ThinkingConfig(include_thoughts=True)
+#     ),
+# )
+
+# for part in response.candidates[0].content.parts:
+#     if not part.text:
+#         continue
+#     if part.thought:
+#         print("Thought summary:")
+#         print(part.text)
+#         print()
+#     else:
+#         print("Answer:")
+#         print(part.text)
+#         print()
+
+# from google import genai
+# from google.genai import types
+
+# client = genai.Client()
+
+# prompt = """
+# Alice, Bob, and Carol each live in a different house on the same street: red, green, and blue.
+# The person who lives in the red house owns a cat.
+# Bob does not live in the green house.
+# Carol owns a dog.
+# The green house is to the left of the red house.
+# Alice does not own a cat.
+# Who lives in each house, and what pet do they own?
+# """
+
+# thoughts = ""
+# answer = ""
+
+# for chunk in client.models.generate_content_stream(
+#     model="gemini-2.5-pro",
+#     contents=prompt,
+#     config=types.GenerateContentConfig(
+#         thinking_config=types.ThinkingConfig(include_thoughts=True)
+#     ),
+# ):
+#     for part in chunk.candidates[0].content.parts:
+#         if not part.text:
+#             continue
+#         elif part.thought:
+#             if not thoughts:
+#                 print("Thoughts summary:")
+#             print(part.text)
+#             thoughts += part.text
+#         else:
+#             if not answer:
+#                 print("Answer:")
+#             print(part.text)
+#             answer += part.text
+
+# from google import genai
+# from google.genai import types
+
+# client = genai.Client()
+
+# response = client.models.generate_content(
+#     model="gemini-3-flash-preview",
+#     contents="Provide a list of 3 famous physicists and their key contributions",
+#     config=types.GenerateContentConfig(
+#         thinking_config=types.ThinkingConfig(thinking_level="low")
+#     ),
+# )
+
+# print(response.text)
+
+# Use dynamic thinking?
+
+
+# from google import genai
+# from google.genai import types
+
+# client = genai.Client()
+
+# response = client.models.generate_content(
+#     model="gemini-2.5-flash",
+#     contents="Provide a list of 3 famous physicists and their key contributions",
+#     config=types.GenerateContentConfig(
+#         thinking_config=types.ThinkingConfig(thinking_budget=1024)
+#         # Turn off thinking:
+#         # thinking_config=types.ThinkingConfig(thinking_budget=0)
+#         # Turn on dynamic thinking:
+#         # thinking_config=types.ThinkingConfig(thinking_budget=-1)
+#     ),
+# )
+
+# print(response.text)
