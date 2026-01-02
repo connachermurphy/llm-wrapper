733fd48956ee22aa58be6c4d6d9430d42a4f8446
Connacher Murphy <connacher.murphy@gmail.com>
2026-01-02 14:33:59 -0500

    Static request, wrapped

--- src/llm_wrapper/google.py
+++ src/llm_wrapper/google.py
@@ -1,15 +1,21 @@
 from google import genai
+from google.genai import types
 
-from .base import Client
+from .base import Client, LLMResponse, Message
 
 
+# TODO: type response
 def _extract_text_and_reasoning(response) -> tuple[str, str]:
     text = []
     reasoning = []
 
-    # Placeholder for response concatenation
-    text.append("placeholder")
-    reasoning.append("placeholder")
+    for part in response.candidates[0].content.parts:
+        if not part.text:
+            continue
+        if part.thought:
+            reasoning.append(part.text)
+        else:
+            text.append(part.text)
 
     text = "\n".join(text).strip()
     reasoning = "\n".join(reasoning).strip()
@@ -18,7 +24,37 @@ def _extract_text_and_reasoning(response) -> tuple[str, str]:
 
 
 class GoogleClient(Client):
-    def __init__(self, *, model: str) -> None:
-        # Can we initialize with an explicit specification of the API key?
-        # If not, do we need the same footprint?
-        self._client = genai.Client()
+    def __init__(self, *, api_key: str, model: str) -> None:
+        self._client = genai.Client(api_key=api_key)
+        self._model = model
+
+    def generate(
+        self,
+        *,
+        messages: list[Message],
+        system: str | None = None,
+        max_tokens: int = 1024,
+        temperature: float | None = None,
+        reasoning: dict | None = None,
+    ) -> LLMResponse:
+        request = {
+            "model": self._model,
+            "contents": "Please introduce yourself. Please end your response with ':)'",
+            "config": types.GenerateContentConfig(
+                thinking_config=types.ThinkingConfig(
+                    include_thoughts=True, thinking_level="low"
+                )
+            ),
+        }
+
+        # TODO: build request object
+        # TODO: concatenate messages and system prompt into "contents" string
+        # TODO: configure temperature
+
+        response = self._client.models.generate_content(**request)
+
+        response_text, response_reasoning = _extract_text_and_reasoning(response)
+
+        return LLMResponse(
+            text=response_text, reasoning=response_reasoning, raw=response
+        )
