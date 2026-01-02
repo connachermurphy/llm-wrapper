b7c3143ca6a0a70ab86ee0996c59ebc5e61ad558
Connacher Murphy <connacher.murphy@gmail.com>
2026-01-02 13:43:06 -0500

    Concatenate response components in unwrapped google example

--- examples/unwrapped_google.py
+++ examples/unwrapped_google.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 from dotenv import load_dotenv
 from google import genai
@@ -6,16 +7,14 @@ from google.genai import types
 
 logging.basicConfig(level=logging.INFO)
 
-# TODO: can we explicitly initialize the client with an API key?
-# TODO: parse thought and content from response object
-# TODO: switch to interactions API?
-
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
 
     load_dotenv()
 
-    client = genai.Client()
+    api_key = os.environ.get("GOOGLE_API_KEY")
+
+    client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
         model="gemini-3-flash-preview",
@@ -28,52 +27,28 @@ if __name__ == "__main__":
         ),
     )
 
-    # Print text and thought (for now)
+    # Report full response
+    logger.info("Full response:")
+    logger.info(response)
+
+    # Initialize text and reasoning strings
+    text = []
+    reasoning = []
+
+    # Extract text and reasoning
     for part in response.candidates[0].content.parts:
         if not part.text:
             continue
         if part.thought:
-            print("Thought: ", part.text)
+            reasoning.append(part.text)
         else:
-            print("Text: ", part.text)
+            text.append(part.text)
 
+    text = "\n".join(text).strip()
+    reasoning = "\n".join(reasoning).strip()
 
-# response = client.models.generate_content(
-#     model="gemini-2.5-flash",
-#     contents="Provide a list of 3 famous physicists and their key contributions",
-#     config=types.GenerateContentConfig(
-#         thinking_config=types.ThinkingConfig(thinking_budget=1024)
-#         # Turn off thinking:
-#         # thinking_config=types.ThinkingConfig(thinking_budget=0)
-#         # Turn on dynamic thinking:
-#         # thinking_config=types.ThinkingConfig(thinking_budget=-1)
-#     ),
-# )
+    logger.info("Text:")
+    logger.info(text)
 
-# print(response.text)
-
-# Example of client-side state management:
-# from google import genai
-
-# client = genai.Client()
-
-# conversation_history = [
-#     {"role": "user", "content": "What are the three largest cities in Spain?"}
-# ]
-
-# interaction1 = client.interactions.create(
-#     model="gemini-3-flash-preview", input=conversation_history
-# )
-
-# print(f"Model: {interaction1.outputs[-1].text}")
-
-# conversation_history.append({"role": "model", "content": interaction1.outputs})
-# conversation_history.append(
-#     {"role": "user", "content": "What is the most famous landmark in the second one?"}
-# )
-
-# interaction2 = client.interactions.create(
-#     model="gemini-3-flash-preview", input=conversation_history
-# )
-
-# print(f"Model: {interaction2.outputs[-1].text}")
+    logger.info("Reasoning:")
+    logger.info(reasoning)
